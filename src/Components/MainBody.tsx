e2ce43a31f8ee1c58473c0c1039f16bb444c0d9b
AchiGel <ar.gelashvili@gmail.com>
2024-03-05 13:50:15 +0400

    add main body styles

--- src/Components/MainBody.tsx
+++ src/Components/MainBody.tsx
@@ -1,7 +1,60 @@
 import "../styles/mainBody.css";
 
 function MainBody() {
-  return <section className="main-body">Main Body</section>;
+  return (
+    <section className="main-body">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum ipsa
+      ipsum, dolor nihil cupiditate voluptatem esse quidem commodi officia?
+      Deserunt aut officia consequuntur veritatis incidunt atque ipsam corporis
+      esse. Repellendus? Lorem ipsum dolor sit, amet consectetur adipisicing
+      elit. Nostrum ipsa ipsum, dolor nihil cupiditate voluptatem esse quidem
+      commodi officia? Deserunt aut officia consequuntur veritatis incidunt
+      atque ipsam corporis esse. Repellendus? Lorem ipsum dolor sit, amet
+      consectetur adipisicing elit. Nostrum ipsa ipsum, dolor nihil cupiditate
+      voluptatem esse quidem commodi officia? Deserunt aut officia consequuntur
+      veritatis incidunt atque ipsam corporis esse. Repellendus? Lorem ipsum
+      dolor sit, amet consectetur adipisicing elit. Nostrum ipsa ipsum, dolor
+      nihil cupiditate voluptatem esse quidem commodi officia? Deserunt aut
+      officia consequuntur veritatis incidunt atque ipsam corporis esse.
+      Repellendus? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Nostrum ipsa ipsum, dolor nihil cupiditate voluptatem esse quidem commodi
+      officia? Deserunt aut officia consequuntur veritatis incidunt atque ipsam
+      corporis esse. Repellendus? Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Nostrum ipsa ipsum, dolor nihil cupiditate voluptatem
+      esse quidem commodi officia? Deserunt aut officia consequuntur veritatis
+      incidunt atque ipsam corporis esse. Repellendus? Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Nostrum ipsa ipsum, dolor nihil
+      cupiditate voluptatem esse quidem commodi officia? Deserunt aut officia
+      consequuntur veritatis incidunt atque ipsam corporis esse. Repellendus?
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum ipsa
+      ipsum, dolor nihil cupiditate voluptatem esse quidem commodi officia?
+      Deserunt aut officia consequuntur veritatis incidunt atque ipsam corporis
+      esse. Repellendus? Lorem ipsum dolor sit, amet consectetur adipisicing
+      elit. Nostrum ipsa ipsum, dolor nihil cupiditate voluptatem esse quidem
+      commodi officia? Deserunt aut officia consequuntur veritatis incidunt
+      atque ipsam corporis esse. Repellendus? Lorem ipsum dolor sit, amet
+      consectetur adipisicing elit. Nostrum ipsa ipsum, dolor nihil cupiditate
+      voluptatem esse quidem commodi officia? Deserunt aut officia consequuntur
+      veritatis incidunt atque ipsam corporis esse. Repellendus? Lorem ipsum
+      dolor sit, amet consectetur adipisicing elit. Nostrum ipsa ipsum, dolor
+      nihil cupiditate voluptatem esse quidem commodi officia? Deserunt aut
+      officia consequuntur veritatis incidunt atque ipsam corporis esse.
+      Repellendus? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Nostrum ipsa ipsum, dolor nihil cupiditate voluptatem esse quidem commodi
+      officia? Deserunt aut officia consequuntur veritatis incidunt atque ipsam
+      corporis esse. Repellendus? Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. Nostrum ipsa ipsum, dolor nihil cupiditate voluptatem
+      esse quidem commodi officia? Deserunt aut officia consequuntur veritatis
+      incidunt atque ipsam corporis esse. Repellendus? Lorem ipsum dolor sit,
+      amet consectetur adipisicing elit. Nostrum ipsa ipsum, dolor nihil
+      cupiditate voluptatem esse quidem commodi officia? Deserunt aut officia
+      consequuntur veritatis incidunt atque ipsam corporis esse. Repellendus?
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum ipsa
+      ipsum, dolor nihil cupiditate voluptatem esse quidem commodi officia?
+      Deserunt aut officia consequuntur veritatis incidunt atque ipsam corporis
+      esse. Repellendus?
+    </section>
+  );
 }
 
 export default MainBody;
