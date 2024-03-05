a04101028264cfc1336b96abbb7d24dace1023e2
AchiGel <ar.gelashvili@gmail.com>
2024-03-05 12:57:09 +0400

    add suggested profiles

--- src/Components/SuggestedProfiles.tsx
+++ src/Components/SuggestedProfiles.tsx
@@ -1,5 +1,22 @@
+import "../styles/suggestedProfiles.css";
+import AsideProfile from "./AsideProfile";
+
 function SuggestedProfiles() {
-  return <div></div>;
+  return (
+    <div className="suggested-profiles">
+      <div className="suggested-profiles-header">
+        <h2>Suggested for you</h2>
+        <button>See All</button>
+      </div>
+      <div className="suggested-profiles-cards">
+        <AsideProfile />
+        <AsideProfile />
+        <AsideProfile />
+        <AsideProfile />
+        <AsideProfile />
+      </div>
+    </div>
+  );
 }
 
 export default SuggestedProfiles;
