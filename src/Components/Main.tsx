1682326fc2fefb0b8556d84152777a68ebb3ff1b
AchiGel <ar.gelashvili@gmail.com>
2024-03-08 15:51:16 +0400

    add fetching functions

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,29 +1,27 @@
 import MainAside from "./MainAside";
 import MainBody from "./MainBody";
 import "../styles/main.css";
+import { useEffect, useState } from "react";
 
 function Main(props: any) {
-  const contentArray = [
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-    "https://picsum.photos/300/300",
-  ];
+  const [contentArray, setContentArray] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetchRandomImages();
+  }, []);
+
+  const fetchRandomImages = async () => {
+    try {
+      const response = await fetch(
+        "https://picsum.photos/v2/list?page=1&limit=20"
+      );
+      const data = await response.json();
+      const links = data.map((item: any) => item.download_url);
+      setContentArray(links);
+    } catch (error) {
+      console.error("Error fetching images:", error);
+    }
+  };
   return (
     <main className="main">
       {!props.exploreClicked ? (
--- src/Components/MainBody.tsx
+++ src/Components/MainBody.tsx
@@ -1,20 +1,27 @@
+import { useEffect, useState } from "react";
 import "../styles/mainBody.css";
 import Post from "./Post";
 
 function MainBody() {
-  const postContent = [
-    { postImage: "https://picsum.photos/470/700" },
-    { postImage: "https://picsum.photos/470/700" },
-    { postImage: "https://picsum.photos/470/700" },
-    { postImage: "https://picsum.photos/470/700" },
-    { postImage: "https://picsum.photos/470/700" },
-    { postImage: "https://picsum.photos/470/700" },
-  ];
+  const [postContent, setPostContent] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetchingImages();
+  }, []);
+
+  async function fetchingImages() {
+    const response = await fetch("https://picsum.photos/v2/list");
+    const data = await response.json();
+    const links = data.map(
+      (link: { download_url: string }) => link.download_url
+    );
+    setPostContent(links);
+  }
 
   return (
     <section className="main-body">
       {postContent.map((item, index) => {
-        return <Post key={index} imgSrc={item.postImage} />;
+        return <Post key={index} imgSrc={item} />;
       })}
     </section>
   );
